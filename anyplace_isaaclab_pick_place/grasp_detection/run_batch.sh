2aefda5586a57ef28cfdd64400aa6b3a678d1f90
nickluocy <1787470600@qq.com>
2025-03-11 23:24:47 +0800

    add missing patch files and update readme for clarification

--- anyplace_isaaclab_pick_place/grasp_detection/run_batch.sh
+++
@@ -1,9 +0,0 @@
-source ~/.bashrc
-
-exps=($(ls ~/gpp/general_pick_place/isaac_lab_pick_place/data/hanging | grep scaled))
-
-for exp in "${exps[@]}"
-do
-  python main.py --exp ~/gpp/general_pick_place/isaac_lab_pick_place/data/hanging/$exp
-done
-
