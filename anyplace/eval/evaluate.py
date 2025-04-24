5db5713c63cba59d82eac0fc135891d3c6018562
jiahexu <xjh4438318846@gmail.com>
2025-04-24 11:51:35 -0400

    update

--- anyplace/eval/evaluate.py
+++ anyplace/eval/evaluate.py
@@ -164,91 +164,97 @@ def main(args: config_util.AttrDict) -> None:
     if util.exists_and_true(exp_args.eval, 'multi_aff_rot'):
         infer_kwargs['multi_aff_rot'] = True
 
-    eval_dataset_path = args.experiment.eval.eval_dataset_path
-    eval_dataset_list = sorted( [os.path.join(eval_dataset_path, f) for f in os.listdir(eval_dataset_path) if os.path.isfile(os.path.join(eval_dataset_path, f))] )
+    tasks = ['hang_mug', 'stack_blocks', 'pour_into_bowl', 'push_T_hex_key', 'close_lid']
+    for task in tasks:
+        eval_dataset_path = args.experiment.eval.eval_dataset_path + task + '_test_data'
+        eval_dataset_list = sorted( [os.path.join(eval_dataset_path, f) for f in os.listdir(eval_dataset_path) if os.path.isfile(os.path.join(eval_dataset_path, f))] )
 
-    print("eval_dataset_list: ", eval_dataset_list)
-    num_clons = exp_args.eval.init_k_val 
-    for data_path in eval_dataset_list:
-        # print("data: ", data_path)
-        print(f'!!!!!!!!! Running inference for {data_path} !!!!!!!!')
-        # print(str(data_path)[-6:-4])
+        print("eval_dataset_list: ", eval_dataset_list)
+        num_clons = exp_args.eval.init_k_val 
+        for data_path in eval_dataset_list:
+            print("data: ", data_path)
+            # task_name = data_path.split('/')[-2]
+            # print("task_name: ", task_name)
+            # print("task_name: ", task_name)
+            # print("task_name: ", task_name)
+            print(f'!!!!!!!!! Running inference for {data_path} !!!!!!!!')
+            # print(str(data_path)[-6:-4])
 
-        # print(f"./anyplace_result/test{str(data_path)[-6:-4]}.npy",)
-        exp_args.num_iterations = 1
+            # print(f"./anyplace_result/test{str(data_path)[-6:-4]}.npy",)
+            exp_args.num_iterations = 1
 
-        use_molmo_crop = False   # this is to prevent cropping the base object like pegs in insertion eval
-        episode = np.load(data_path , allow_pickle=True)
-        episode = episode.item()
-        
-        base_obj_pc = numpy_2_pcd(episode['pcd'][0]).transform( get_transform(episode['world_frame_pose'][0]) )
-        base_obj_pc = pcd_2_numpy( base_obj_pc )
-
-        grasp_obj_pc = numpy_2_pcd(episode['pcd'][1]).transform( get_transform(episode['world_frame_pose'][1]) )
-        grasp_obj_pc = pcd_2_numpy( grasp_obj_pc )
-
-        # Todo change to world frame
-
-        for iteration in range(exp_args.start_iteration, exp_args.num_iterations):
-
-            crop_base_obj_pc = base_obj_pc
-            num_clons = 100  # for object that does not need cropping, do 100 iterations
-
-            if crop_base_obj_pc.shape[0] < 10:
-                continue
- 
+            use_molmo_crop = False   # this is to prevent cropping the base object like pegs in insertion eval
+            episode = np.load(data_path , allow_pickle=True)
+            episode = episode.item()
             
-            scene_extents = args.data.coarse_aff.scene_extents 
-            scene_scale = 1 / np.max(scene_extents)
+            base_obj_pc = numpy_2_pcd(episode['pcd'][0]).transform( get_transform(episode['world_frame_pose'][0]) )
+            base_obj_pc = pcd_2_numpy( base_obj_pc )
 
-            args.data.coarse_aff.scene_scale = scene_scale 
-            infer_kwargs['gt_child_cent'] = None
-            infer_kwargs['export_viz'] = args.export_viz
-            infer_kwargs['export_viz_dirname'] = args.export_viz_dirname
-            infer_kwargs['export_viz_relative_trans_guess'] = None
-            infer_kwargs['iteration'] = iteration
-            
-            multi_mesh_dict = None
+            grasp_obj_pc = numpy_2_pcd(episode['pcd'][1]).transform( get_transform(episode['world_frame_pose'][1]) )
+            grasp_obj_pc = pcd_2_numpy( grasp_obj_pc )
 
-            #base_obj_pc  = crop_base_obj_pc
-            parent_pcd = base_obj_pc
-            child_pcd_guess = grasp_obj_pc
-            relative_trans_preds = infer_relation_policy(
-                mc_vis, 
-                parent_pcd, child_pcd_guess, 
-                coarse_aff_model,
-                pr_model, 
-                success_model,
-                scene_mean=args.data.coarse_aff.scene_mean, scene_scale=args.data.coarse_aff.scene_scale, 
-                grid_pts=raster_pts, rot_grid=rot_grid, 
-                viz=False, n_iters=exp_args.eval.n_refine_iters, 
-                no_parent_crop=(not exp_args.parent_crop),
-                return_top=(not exp_args.eval.return_rand), with_coll=exp_args.eval.with_coll, 
-                run_affordance=exp_args.eval.run_affordance, init_k_val=num_clons,
-                no_sc_score=exp_args.eval.no_success_classifier, 
-                init_parent_mean=exp_args.eval.init_parent_mean_pos, init_orig_ori=exp_args.eval.init_orig_ori,
-                refine_anneal=exp_args.eval.refine_anneal,
-                mesh_dict=multi_mesh_dict,
-                add_per_iter_noise=exp_args.eval.add_per_iter_noise,
-                per_iter_noise_kwargs=exp_args.eval.per_iter_noise_kwargs,
-                variable_size_crop=exp_args.eval.variable_size_crop,
-                timestep_emb_decay_factor=exp_args.eval.timestep_emb_decay_factor,
-                remove_redundant_pose = args.experiment.eval.remove_redundant_pose,
-                **infer_kwargs)
+            # Todo change to world frame
 
-            # save data
-            data = {"relative": relative_trans_preds}
-            # print("output: ", relative_trans_preds.shape)
+            for iteration in range(exp_args.start_iteration, exp_args.num_iterations):
 
-            # save data
-            result = copy.deepcopy(episode)
-            print("relative_trans_preds: ", )
-            result['anyplace_result'] = relative_trans_preds
-            save_data_dir = "./anyplace_result"
-            OUTPUT_DIR = Path(save_data_dir)
-            OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+                crop_base_obj_pc = base_obj_pc
+                num_clons = 100  # for object that does not need cropping, do 100 iterations
 
-            np.save(f"./anyplace_result/test{str(data_path)[-6:-4]}.npy", result, allow_pickle = True)
+                if crop_base_obj_pc.shape[0] < 10:
+                    continue
+    
+                
+                scene_extents = args.data.coarse_aff.scene_extents 
+                scene_scale = 1 / np.max(scene_extents)
+
+                args.data.coarse_aff.scene_scale = scene_scale 
+                infer_kwargs['gt_child_cent'] = None
+                infer_kwargs['export_viz'] = args.export_viz
+                infer_kwargs['export_viz_dirname'] = args.export_viz_dirname
+                infer_kwargs['export_viz_relative_trans_guess'] = None
+                infer_kwargs['iteration'] = iteration
+                
+                multi_mesh_dict = None
+
+                #base_obj_pc  = crop_base_obj_pc
+                parent_pcd = base_obj_pc
+                child_pcd_guess = grasp_obj_pc
+                relative_trans_preds = infer_relation_policy(
+                    mc_vis, 
+                    parent_pcd, child_pcd_guess, 
+                    coarse_aff_model,
+                    pr_model, 
+                    success_model,
+                    scene_mean=args.data.coarse_aff.scene_mean, scene_scale=args.data.coarse_aff.scene_scale, 
+                    grid_pts=raster_pts, rot_grid=rot_grid, 
+                    viz=False, n_iters=exp_args.eval.n_refine_iters, 
+                    no_parent_crop=(not exp_args.parent_crop),
+                    return_top=(not exp_args.eval.return_rand), with_coll=exp_args.eval.with_coll, 
+                    run_affordance=exp_args.eval.run_affordance, init_k_val=num_clons,
+                    no_sc_score=exp_args.eval.no_success_classifier, 
+                    init_parent_mean=exp_args.eval.init_parent_mean_pos, init_orig_ori=exp_args.eval.init_orig_ori,
+                    refine_anneal=exp_args.eval.refine_anneal,
+                    mesh_dict=multi_mesh_dict,
+                    add_per_iter_noise=exp_args.eval.add_per_iter_noise,
+                    per_iter_noise_kwargs=exp_args.eval.per_iter_noise_kwargs,
+                    variable_size_crop=exp_args.eval.variable_size_crop,
+                    timestep_emb_decay_factor=exp_args.eval.timestep_emb_decay_factor,
+                    remove_redundant_pose = args.experiment.eval.remove_redundant_pose,
+                    **infer_kwargs)
+
+                # save data
+                data = {"relative": relative_trans_preds}
+                # print("output: ", relative_trans_preds.shape)
+
+                # save data
+                result = copy.deepcopy(episode)
+                print("relative_trans_preds: ", )
+                result['anyplace_result'] = relative_trans_preds
+                save_data_dir = "/home/jiahe/anyplace_result/" + task + '/' 
+                OUTPUT_DIR = Path(save_data_dir)
+                OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+
+                np.save(  save_data_dir+ f"test{str(data_path)[-6:-4]}.npy", result, allow_pickle = True)
 
 
 
